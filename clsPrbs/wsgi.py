d5d9073626bacb1e25f0b92f0eb1853f10128e07
kubos777 <chd.jl@hotmail.com>
2018-04-09 05:43:52 +0000

    Se montó el ejemplo en pythonanywhere

--- clsPrbs/wsgi.py
+++ clsPrbs/wsgi.py
@@ -14,3 +14,30 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "clsPrbs.settings")
 
 application = get_wsgi_application()
+
+
+
+'''
+
+
+Archivo WSGI de configuracion en pythonanywhere:
+from django.core.wsgi import get_wsgi_application
+import	os
+import	sys
+
+path	=	'/home/kubos777/clsPrbs'
+if	path	not	in	sys.path:
+	sys.path.append(path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'clsPrbs.settings'
+
+#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "clsPrbs.settings")
+
+#application = get_wsgi_application()
+
+from	whitenoise.django	import	DjangoWhiteNoise
+
+application	=	DjangoWhiteNoise(get_wsgi_application())
+
+'''
+
