aaa1b76cfc48efbab4e35fc9b27506d032222456
kubos777 <chd.jl@hotmail.com>
2018-04-09 01:32:33 -0500

    Agrego la salida dinámica de los post al template, mediante el ORM

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-
+from django.utils import timezone
+from .models import Post
 # Create your views here.
 
 def listadoPosts(request):
-    return render(request,'blog/listadoPost.html',{})
+    posts= Post.objects.filter(fechaPublicacion__lte =timezone.now()).order_by('fechaPublicacion')
+    return render(request,'blog/listadoPost.html',{'posts':posts})
 
