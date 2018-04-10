f35737ab3cef8677bf907130dde93b3ab0fe24d5
kubos777 <chd.jl@hotmail.com>
2018-04-10 01:42:49 -0500

    Finalizo blog, añadí la opción de editar y agregar nuevo post. Faltaría implementar el inicio con otro usuario y borrar post

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,9 @@ from django.urls import path,include
 from . import views
 
 urlpatterns = [
-    path('', views.listadoPosts),
+    path('', views.listadoPosts,name='inicio'),
     path('post/<pk>',views.detalles,name='detalles'),
+    path('post/nuevo/',views.nuevoPost,name = 'nuevoPost'),
+    path('post/<pk>/modificar/', views.modificar, name='modificar'),
+
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render,get_object_or_404,redirect
 from django.utils import timezone
 from .models import Post
+from .forms import PostFormulario
 
 # Create your views here.
 
@@ -11,3 +12,29 @@ def listadoPosts(request):
 def detalles(request,pk):
     post = get_object_or_404(Post,pk = pk)
     return render(request,'blog/detalles.html',{'post':post})
+
+def nuevoPost(request):
+    if request.method == 'POST':
+        form = PostFormulario(request.POST)
+        if  form.is_valid():
+            post = form.save(commit=False)
+            post.autor = request.user
+            post.fechaPublicacion = timezone.now()
+            post.save()
+            return redirect('detalles',pk=post.pk)
+    else:
+        form = PostFormulario()
+    return render(request, 'blog/editar.html',{'form':form})
+
+def modificar(request,pk):
+    post = get_object_or_404(Post,pk=pk)
+    if request.method == 'POST':
+        form = PostFormulario(request.POST,instance=post)
+        if  form.is_valid():
+            post = form.save(commit=False)
+            post.autor = request.user
+            post.save()
+            return redirect('detalles',pk=post.pk)
+    else:
+        form = PostFormulario(instance=post)
+    return render(request, 'blog/editar.html',{'form':form})
