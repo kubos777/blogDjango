d0ef3dfc7c3177cc06031f3f6c625c4d706fe60f
kubos777 <chd.jl@hotmail.com>
2018-04-10 00:51:08 -0500

    Agregué base.html para todos los templates. Y cree la url para los detalles.

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path,include
 from . import views
 
 urlpatterns = [
-    path('', views.listadoPosts)
+    path('', views.listadoPosts),
+    path('post/<pk>',views.detalles,name='detalles'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from django.utils import timezone
 from .models import Post
+
 # Create your views here.
 
 def listadoPosts(request):
     posts= Post.objects.filter(fechaPublicacion__lte =timezone.now()).order_by('fechaPublicacion')
     return render(request,'blog/listadoPost.html',{'posts':posts})
 
+def detalles(request,pk):
+    post = get_object_or_404(Post,pk = pk)
+    return render(request,'blog/detalles.html',{'post':post})
